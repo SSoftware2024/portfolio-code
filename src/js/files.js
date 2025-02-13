9cbf76a1ef19f0646c9d28f552178943d1a7f91e
Soluções Software <ssolucoessoftware@gmail.com>
2025-02-12 21:08:12 -0300

    texto de experiência

--- src/js/files.js
+++ src/js/files.js
@@ -1,6 +1,10 @@
 import profile from '../assets/perfil.png';
 import curriculo from '../assets/Curriculo_Tiago_Alves.pdf';
+import logo_dc from '../assets/dc.png';
+import logo_vwp from '../assets/vwp.jpeg';
 export{
     profile,
-    curriculo
+    curriculo,
+    logo_dc,
+    logo_vwp
 };
